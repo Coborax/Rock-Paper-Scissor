0d28fc480217d6f68f57bfcc761be97cce7b1cbb
Mikkel Mouridsen <mikkelmouridsen@protonmail.com>
2021-03-03 13:57:20 +0100

    Added basic bot logic

--- src/rps/bll/player/Player.java
+++ src/rps/bll/player/Player.java
@@ -7,6 +7,7 @@ import rps.bll.game.Result;
 
 //Java imports
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Example implementation of a player.
@@ -49,7 +50,43 @@ public class Player implements IPlayer {
         //Historic data to analyze and decide next move...
         ArrayList<Result> results = (ArrayList<Result>) state.getHistoricResults();
 
-        //TODO: Fancy logic or something
-        return Move.Rock;
+        //Get a random move if there are no results yet
+        if (results.isEmpty()) {
+            return getRandomMove(Move.values());
+        }
+
+        Result lastResult = results.get(results.size() - 1);
+        Move playerLastMove;
+
+        //Get the players last move
+        if (lastResult.getWinnerPlayer().getPlayerType() == PlayerType.Human) {
+            playerLastMove = lastResult.getWinnerMove();
+        } else {
+            playerLastMove = lastResult.getLoserMove();
+        }
+
+        // Decide the new move, based on the players last move. (Will pick a random of the two)
+        // +=============+=================================+=================================+
+        // | Player Move | Player possible move (Bot Move) | Player possible move (Bot Move) |
+        // +=============+=================================+=================================+
+        // | Rock        | Scissor (Rock)                  | Paper (Scissor)                 |
+        // +-------------+---------------------------------+---------------------------------+
+        // | Paper       | Rock (Paper)                    | Scissor (Rock)                  |
+        // +-------------+---------------------------------+---------------------------------+
+        // | Scissor     | Paper (Scissor)                 | Rock (Paper)                    |
+        // +-------------+---------------------------------+---------------------------------+
+        Move move = playerLastMove;
+        switch (playerLastMove) {
+            case Rock -> move = getRandomMove(Move.Rock, Move.Scissor);
+            case Paper -> move = getRandomMove(Move.Paper, Move.Rock);
+            case Scissor -> move = getRandomMove(Move.Scissor, Move.Paper);
+        }
+
+        return move;
+    }
+
+    private Move getRandomMove(Move... moves) {
+        Random rand = new Random();
+        return moves[rand.nextInt(moves.length)];
     }
 }
